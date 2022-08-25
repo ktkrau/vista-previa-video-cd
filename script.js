b78cdc7b899ff0843b6aa4289235755faab89f66
ktkrau <ktkrau@gmail.com>
2022-08-25 15:12:32 -0400

    ejercicio terminado

---
+++ script.js
@@ -0,0 +1,10 @@
+console.log("page loaded...");
+
+function playVideo(element_video){
+    console.log("mouse on");
+    element_video.play()
+}
+function pauseVideo(element_video){
+    console.log("mouse out");
+    element_video.pause()
+}
